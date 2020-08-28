3c587479a74bc8b3a105ae3bc091ec23a2d2b1d2
AlexanderKufeld <alexander.kufeld@gmail.com>
2020-08-28 12:09:52 +0200

    solution for enter_event

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-app';
+  greeting = 'Hello, ';
+  specificName = '';
+  initialValue = 'World';
+
+  keyupHandler(event: KeyboardEvent, name: string): void {
+
+    //console.log(event);
+    if (event.key === 'Enter') {
+      this.specificName = name;
+    }
+  }
+}
