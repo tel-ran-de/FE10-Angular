f8d23abbaa532f8844b62e8bb9a432990a1fe8fa
Alex Loginov <is.alex.loginov@gmail.com>
2020-08-31 19:17:21 +0200

    HW 2020-08-31 Add city-weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CityTileComponent } from './city-tile/city-tile.component';
+import { CityWeatherComponent } from './city-weather/city-weather.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CityTileComponent
+    CityTileComponent,
+    CityWeatherComponent
   ],
   imports: [
     BrowserModule,
