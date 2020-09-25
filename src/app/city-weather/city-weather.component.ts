3d845c855fec48e3071fc9c6b3ee3fb09602a376
maximorlenco <maxim.orlenco@gmail.com>
2020-09-25 19:23:38 +0200

    Functionality for deleting City , adding Card to component

--- src/app/city-weather/city-weather.component.ts
+++ src/app/city-weather/city-weather.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CityModel} from '../model/CityModel';
+import {CityService} from '../services/city.service';
+import {CityDto} from '../dto/CityDto';
+import {flatMap} from 'rxjs/operators';
+import {WeatherService} from '../services/weather.service';
+import {any} from 'codelyzer/util/function';
 
 @Component({
   selector: 'app-city-weather',
@@ -8,14 +14,34 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class CityWeatherComponent implements OnInit {
 
-  city: string;
+  city: CityModel = {
+    name: null,
+    imageUrl: null,
+    temp: null
 
-  constructor(private route: ActivatedRoute) { }
+  };
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private cityService: CityService,
+              private weatherService: WeatherService) {
+  }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      this.city = params.city;
-    });
+    this.route.params.pipe(
+      flatMap(params => this.cityService.getCitiesByName(params.name))
+    ).subscribe((city: CityDto) =>
+      this.weatherService.getWeather(city.name)
+        .subscribe(data => {
+            this.city.name = city.name;
+            this.city.imageUrl = city.imageUrl;
+            this.city.temp = data.main.temp;
+          }
+        ));
+  }
+  onClick(): void {
+    this.cityService.removeCity(this.city.name);
+    this.router.navigate(['']);
   }
 
 }
